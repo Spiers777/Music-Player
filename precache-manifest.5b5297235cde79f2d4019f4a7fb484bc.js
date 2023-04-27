fd86bc9edfe52312dcce79712b13705bd4016fd3
Spiers777 <isaacspiers7@gmail.com>
2023-04-27 22:22:16 +0100

    Updates

---
+++ precache-manifest.5b5297235cde79f2d4019f4a7fb484bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99309e03a76d84df329ad06c442486fb",
+    "url": "/Music-Player/index.html"
+  },
+  {
+    "revision": "e132584700f73618137f",
+    "url": "/Music-Player/static/css/main.ca5e3232.chunk.css"
+  },
+  {
+    "revision": "dea9ef75d06e7bf336a6",
+    "url": "/Music-Player/static/js/2.81b60536.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Music-Player/static/js/2.81b60536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e132584700f73618137f",
+    "url": "/Music-Player/static/js/main.3e0ee3a1.chunk.js"
+  },
+  {
+    "revision": "a1c0a8b553d7ea26845f",
+    "url": "/Music-Player/static/js/runtime-main.5f1127e3.js"
+  }
+]);
